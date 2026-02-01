4866a055b3f9e69106b4673172bf6c6463c32073
Davidsoumare25 <soumaredaouda784@gmail.com>
2026-02-01 08:52:01 +0000

    Create home.js

---
+++ home.js
@@ -0,0 +1,9 @@
+// bouton publier (test)
+document.getElementById("publish-btn").onclick = () => {
+  alert("Publier fonctionne 👍");
+};
+
+// bouton déconnexion
+document.getElementById("logout-btn").onclick = () => {
+  window.location.href = "index.html";
+};
